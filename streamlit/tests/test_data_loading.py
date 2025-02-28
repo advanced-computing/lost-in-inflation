5fa6e6462bb62562b95fc6dcf686e16feab0196c
Ibrahim Alangari <ika2106@columbia.edu>
2025-02-28 15:50:37 -0500

    testpy

--- streamlit/tests/test_data_loading.py
+++ streamlit/tests/test_data_loading.py
@@ -2,26 +2,23 @@ import pytest
 from my_streamlit_app import load_csv_data
 
 @pytest.fixture
-def test_pce():
-    df = load_csv_data("./streamlit/MoM PCE.csv", drop_cols=['Year', 'Month'], create_date_from=['Year', 'Month'])
-    return df
+def pce_df():
+    return load_csv_data("MoM PCE.csv", drop_cols=['Year', 'Month'], create_date_from=['Year', 'Month'])
 
 @pytest.fixture
-def test_china_mxp():
-    df = load_csv_data("./streamlit/EIUCOCHNTOT.csv", period_col="Period", drop_cols=['Year', 'Period'], start_date="2018-01-01")
-    return df
+def china_mxp_df():
+    return load_csv_data("EIUCOCHNTOT.csv", period_col="Period", drop_cols=['Year', 'Period'], start_date="2018-01-01")
 
 @pytest.fixture
-def test_df():
-    df = load_csv_data("./streamlit/monthly-inflation-data.csv", index_col="Label", date_col="Label")
-    return df
+def inflation_df():
+    return load_csv_data("monthly-inflation-data.csv", index_col="Label", date_col="Label")
 
-def test_dataframes(test_pce, test_china_mxp, test_df):
-    assert not test_df.empty, "Fed inflation data (df) is empty!"
-    assert not test_pce.empty, "PCE data is empty!"
-    assert not test_china_mxp.empty, "China MXP data is empty!"
+def test_dataframes(pce_df, china_mxp_df, inflation_df):
+    assert not inflation_df.empty, "❌ Fed inflation data (df) is empty!"
+    assert not pce_df.empty, "❌ PCE data is empty!"
+    assert not china_mxp_df.empty, "❌ China MXP data is empty!"
 
-    assert "Date" in test_pce.columns, "PCE data is missing Date column!"
-    assert "Date" in test_china_mxp.columns, "China MXP data is missing Date column!"
+    assert "Date" in pce_df.columns, "❌ PCE data is missing 'Date' column!"
+    assert "Date" in china_mxp_df.columns, "❌ China MXP data is missing 'Date' column!"
 
     print("✅ All CSV data loaded correctly!")
