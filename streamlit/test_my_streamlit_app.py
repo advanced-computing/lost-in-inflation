f763b90c47f47c22c296968735239b51974a219e
Ibrahim Alangari <ika2106@columbia.edu>
2025-02-28 17:19:50 -0500

    changing \ to /

--- streamlit/test_my_streamlit_app.py
+++ streamlit/test_my_streamlit_app.py
@@ -3,15 +3,15 @@ from my_streamlit_app import load_csv_data
 
 @pytest.fixture
 def pce_df():
-    return load_csv_data("streamlit\MoM PCE.csv", drop_cols=['Year', 'Month'], create_date_from=['Year', 'Month'])
+    return load_csv_data("streamlit/MoM PCE.csv", drop_cols=['Year', 'Month'], create_date_from=['Year', 'Month'])
 
 @pytest.fixture
 def china_mxp_df():
-    return load_csv_data("streamlit\EIUCOCHNTOT.csv", period_col="Period", drop_cols=['Year', 'Period'], start_date="2018-01-01")
+    return load_csv_data("streamlit/EIUCOCHNTOT.csv", period_col="Period", drop_cols=['Year', 'Period'], start_date="2018-01-01")
 
 @pytest.fixture
 def inflation_df():
-    return load_csv_data("streamlit\monthly-inflation-data.csv", index_col="Label", date_col="Label")
+    return load_csv_data("streamlit/monthly-inflation-data.csv", index_col="Label", date_col="Label")
 
 def test_dataframes(pce_df, china_mxp_df, inflation_df):
     assert not inflation_df.empty, "❌ Fed inflation data (df) is empty!"
